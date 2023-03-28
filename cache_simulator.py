3c06aea90dbf894cd6fbf87e7ec5ba18266b6a43
David Hunter <hunter.davi@northeastern.edu>
2023-03-28 06:43:50 -0400

    updates

--- cache_simulator.py
+++ cache_simulator.py
@@ -2,27 +2,87 @@ import column_associative_cache
 import mapped_caches
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
+    l1_hit_rate = []
+    l1_miss_rate = []
+    l2_miss_rate = []
+    l2_hit_rate = []
+    test_format_list = []
+    l1_cache_assoc = [1, 'CA']
+    l1_cache_sizes = [2, 4]
+    l2_cache_assoc = [2, 4]
+    l2_cache_sizes = [8]
 
+    # address_stream_path = r'D:\Classes\ComputerArchitecture\FinalProject\linpack_address_trace_use.out'
     address_stream_path = r'C:\Users\David Hunter\Desktop\tracefile\trace'
     dataset = pd.read_csv(address_stream_path, delimiter=' ')
+    dataset = dataset[dataset.columns[0:2]]
     dataset.columns = ['type', 'element']
-
     dataset = dataset[dataset['type'].isin([0, 1])].reset_index(inplace=False, drop=True)
 
-    cachel1 = mapped_caches.MappedCache(associativity=1, cache_size=8, block_size=32, cache_level='l1')
-    cachel2 = mapped_caches.MappedCache(associativity=2, cache_size=16, block_size=64, cache_level='l2')
+    for l1_assoc in l1_cache_assoc:
+        for l2_assoc in l2_cache_assoc:
+            for l1_size in l1_cache_sizes:
+                for l2_size in l2_cache_sizes:
 
-    address_count = 0
-    for element in dataset.values:
-        address_count += 1
-        if address_count % 10000 == 0:
-            print(f'Processed {address_count} Instructions...')
-        result = cachel1.process_address(type=element[0], input_address=element[1])
+                    if l1_assoc == 'CA':
+                        cachel1 = column_associative_cache.ColumnCache(cache_size=l1_size, block_size=32, cache_level='l1')
+                    elif l1_assoc == 'CALRU':
+                        print(f'CALRU NOT FOUND, DEFAULTING TO CA')
+                        cachel1 = column_associative_cache.ColumnCache(cache_size=l1_size, block_size=32, cache_level='l1')
+                    else:
+                        cachel1 = mapped_caches.MappedCache(associativity=l1_assoc, cache_size=l1_size, block_size=32, cache_level='l1')
 
-        if result == 'miss':  # Check L2, if not there update
-            cachel2.process_address(type=element[0], input_address=element[1])
+                    if l2_assoc == 'CA':
+                        cachel2 = column_associative_cache.ColumnCache(cache_size=l2_size, block_size=64, cache_level='l2')
+                    elif l2_assoc == 'CALRU':
+                        print(f'CALRU NOT FOUND, DEFAULTING TO CA')
+                        cachel2 = column_associative_cache.ColumnCache(cache_size=l2_size, block_size=64, cache_level='l2')
+                    else:
+                        cachel2 = mapped_caches.MappedCache(associativity=l2_assoc, cache_size=l2_size, block_size=64, cache_level='l2')
 
-    cachel1.print_metrics()
-    cachel2.print_metrics()
+                    print(f'Running Test: {l1_assoc=} {l1_size=}, {l2_assoc=} {l2_size=}...')
+                    address_count = 0
+                    for element in dataset.values:
+                        if address_count % 10000 == 0:
+                            print(f'{address_count=}')
+                        address_count += 1
+                        result = cachel1.process_address(type=element[0], input_address=element[1])
+
+                        if result == 'miss':  # Check L2, if not there update
+                            cachel2.process_address(type=element[0], input_address=element[1])
+
+                    l1_hit, l1_miss = cachel1.print_metrics()
+                    l2_hit, l2_miss = cachel2.print_metrics()
+                    l1_hit_rate.append(l1_hit)
+                    l1_miss_rate.append(l1_miss)
+                    l2_hit_rate.append(l2_hit)
+                    l2_miss_rate.append(l2_miss)
+                    test_format_list.append('L1: ' + str(l1_assoc) + '-assoc ' + str(l1_size) + 'KB ' + 'L2: ' + str(l2_assoc) + '-assoc ' + str(l2_size) + 'KB')
+    ind = np.arange(len(test_format_list))
+    width = 0.35
+    fig0 = plt.figure(0, figsize=(12, 9))
+    plt.tight_layout
+    p1 = plt.bar(ind, l1_hit_rate, width)
+    p2 = plt.bar(ind, l1_miss_rate, width, bottom=l1_hit_rate, )
+    plt.title('L1 Cache')
+    plt.ylabel('Rate')
+    plt.xlabel(f'Test, Hit Rates {l1_hit_rate}')
+    plt.yticks(np.arange(0, 1.01, 0.05))
+    plt.xticks(ind, test_format_list)
+    plt.legend((p1[0], p2[0]), ('Hit Rate', 'Miss Rate'))
+
+    fig1 = plt.figure(1, figsize=(12, 9))
+    plt.tight_layout
+    p1 = plt.bar(ind, l2_hit_rate, width)
+    p2 = plt.bar(ind, l2_miss_rate, width, bottom=l2_hit_rate)
+    plt.title('L2 Cache')
+    plt.ylabel('Rate')
+    plt.xlabel(f'Test, Hit Rates {l2_hit_rate}')
+    plt.yticks(np.arange(0, 1.01, 0.05))
+    plt.xticks(ind, test_format_list)
+    plt.legend((p1[0], p2[0]), ('Hit Rate', 'Miss Rate'))
+
+    plt.show()
--- column_associative_cache.py
+++ column_associative_cache.py
@@ -112,3 +112,115 @@ class ColumnCache:
         else:  # HIT
             self.hit_total += 1
             return 'hit'
+
+class LRUColumnCache4:
+    def __init__(self, cache_size = 8, block_size = 32, cache_level = 'l1'):
+        '''
+        :param cache_size: Size of the cachel1, passed in KiloBytes
+        :param block_size: Block size of each cachel1 index, passed in Bytes
+        :param cache_level: Level of the cachel1, Ex: l1, l2, l3... Used for final data outputs
+        '''
+        self.cache_size = cache_size
+        self.block_size = block_size
+
+        # Program is intended to only model an Data cachel1, so other addresses can be ignored
+        self.column_cache = pd.DataFrame(index=range(int((self.cache_size * 1024) / self.block_size)), columns=range(3))
+        self.column_cache.columns = ['Tag', 'Address', 'Rehash']
+        self.number_cache_indexes = len(bin(int((self.cache_size * 1024) / self.block_size))) - 2
+        self.column_cache['Rehash'] = 1
+        self.miss_total = 0
+        self.hit_total = 0
+        self.instr_count = 0
+        self.cache_level = cache_level
+
+    def print_metrics(self):
+        print(f'Results for {self.cache_level} data cache...')
+        print(f'Cache Metrics:\nColumn Associative Cache, Cache Size: {self.cache_size}, Block Size {self.block_size}')
+        print(f'Hit Ratio = {self.hit_total / self.instr_count}')
+        print(f'Miss Ratio = {self.miss_total / self.instr_count}')
+        print(f'\n')
+
+        return self.hit_total/self.instr_count, self.miss_total/self.instr_count
+
+    def process_address(self, type, input_address):
+        self.instr_count += 1 # Update the instruction count
+        block_size = len(bin(self.block_size)) - 2 # -2 for byte addressing
+
+        # Determine if read or write, if write assume write-through
+
+        address = '{:032b}'.format((int(input_address,16))) # Address of interest
+        address_block_removed = address[:-block_size] # Address not including the block
+
+        tag = address_block_removed[:-self.number_cache_indexes] # Tag for cache to memory mapping
+        index0 = int(address_block_removed[-(self.number_cache_indexes-1):],2) # Index for state 0
+
+        if not tag == self.column_cache['Tag'][index0]:  # MISS
+            if self.column_cache['Rehash'][index0] == 1:
+                self.miss_total += 1
+                # Update stored element
+                self.column_cache['Address'][index0] = address
+                self.column_cache['Tag'][index0] = tag
+                # Update Rehash bit
+                self.column_cache['Rehash'][index0] = 0
+                return 'miss'
+            else:
+
+                # Perform bit flip of first bit in index
+                index1 = bin(index0)
+                if index1[2] == '1':
+                    index1 = index1[:2] + '0' + index1[2:]
+                else:
+                    index1 = index1[:2] + '1' + index1[3:]
+                index1 = int(index1, 2)
+
+                # Check if element is stored in the offset portion of the cachel1
+                if not tag == self.column_cache['Tag'][index1]:  # MISS
+
+                    self.miss_total += 1
+                    # Store element in cachel1
+                    self.column_cache['Address'][index1] = address
+                    self.column_cache['Tag'][index1] = tag
+                    self.column_cache['Rehash'][index1] = 0
+
+                    # Perform swap
+                    # Load 1 to temp cachel1
+                    temp_cache_addr = self.column_cache['Address'][index0]
+                    temp_cache_tag = self.column_cache['Tag'][index0]
+                    temp_cache_rehash = self.column_cache['Rehash'][index0]
+
+                    # Swap 2 into 1
+                    self.column_cache['Address'][index0] = self.column_cache['Address'][index1]
+                    self.column_cache['Tag'][index0] = self.column_cache['Tag'][index1]
+                    self.column_cache['Rehash'][index0] = self.column_cache['Rehash'][index1]
+
+                    # Swap temp (1) into 2
+                    self.column_cache['Address'][index1] = temp_cache_addr
+                    self.column_cache['Tag'][index1] = temp_cache_tag
+                    self.column_cache['Rehash'][index1] = temp_cache_rehash
+
+                    return 'miss'
+
+                # Condition for a delayed hit
+                else:  # HIT
+
+                    self.hit_total += 1
+                    # Perform swap
+                    # Load to temp cachel1
+                    temp_cache_addr = self.column_cache['Address'][index0]
+                    temp_cache_tag = self.column_cache['Tag'][index0]
+                    temp_cache_rehash = self.column_cache['Rehash'][index0]
+
+                    # Swap 2 into 1
+                    self.column_cache['Address'][index0] = self.column_cache['Address'][index1]
+                    self.column_cache['Tag'][index0] = self.column_cache['Tag'][index1]
+                    self.column_cache['Rehash'][index0] = self.column_cache['Rehash'][index1]
+
+                    # Swap temp (1) into 2
+                    self.column_cache['Address'][index1] = temp_cache_addr
+                    self.column_cache['Tag'][index1] = temp_cache_tag
+                    self.column_cache['Rehash'][index1] = temp_cache_rehash
+                    return 'hit'
+        # Condition for hit on the first check
+        else:  # HIT
+            self.hit_total += 1
+            return 'hit'
